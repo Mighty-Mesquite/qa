9171033d28707371128e4cd3f0ad73c686693efd
foodforcode <tsai.connie@yahoo.com>
2021-03-16 22:25:14 -0700

    Add model files

---
+++ models/answer.model.js
@@ -0,0 +1,15 @@
+
+const Answer = require('../database_mongo/answers.js');
+
+//CREATE
+Answer.create = (req,)
+
+
+//READ
+Answer.getAll
+
+//UPDATE
+Answer.updateById
+
+//DELETE
+Answer.remove
---
+++ models/question.model.js
@@ -0,0 +1,13 @@
+const Question = require('../database_mongo/question.js');
+
+//CREATE
+Question.create
+
+//READ
+Question.getAll
+
+//UPDATE
+Question.updateById
+
+//DELETE
+Question.remove
