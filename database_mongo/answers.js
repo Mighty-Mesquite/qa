8c970ee7048d8bef1c4e1ae5c35e7957d962b907
foodforcode <tsai.connie@yahoo.com>
2021-03-16 19:29:45 -0700

    Modify mysql schema and mongo models

--- database_mongo/answers.js
+++ database_mongo/answers.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const answerSchema = new Schema({
-  product_id: Number,
   question_id: Number,
   answerer_name: String,
   helpfullness: Number,
--- database_mysql/schema.sql
+++ database_mysql/schema.sql
@@ -10,14 +10,17 @@ USE questions;
 DROP TABLE IF EXISTS `questions`;
 
 CREATE TABLE `questions` (
-  `questions_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `question_date` DATETIME NOT NULL ,
-  `question_body` TEXT(1000) NOT NULL ,
-  `asker_name` VARCHAR(60) NULL ,
-  `product_id` INTEGER NOT NULL ,
+  `question_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `question_date` DATE NOT NULL,
+  `question_time` TIME NOT NULL,
+  `question_body` TEXT(1000) NOT NULL,
+  `asker_name` VARCHAR(60) NULL,
+  `product_id` SMALLINT NOT NULL,
   `question_helpfulness` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `reported` BINARY NOT NULL DEFAULT 0,
-  PRIMARY KEY (`questions_id`)
+  -- Possibly type booolean not null default false?
+  PRIMARY KEY
+  (`questions_id`)
 );
 
 -- ---
@@ -29,9 +32,9 @@ DROP TABLE IF EXISTS `answers`;
 
 CREATE TABLE `answer` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `body` TEXT(1000) NOT NULL ,
-  `date_` DATETIME NOT NULL ,
-  `answerer_name` VARCHAR(60) NOT NULL ,
+  `body` TEXT(1000) NOT NULL,
+  `date_` DATE NOT NULL,
+  `answerer_name` VARCHAR(60) NOT NULL,
   `helpfulness` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
   `questions_id_question` INTEGER NOT NULL,
   PRIMARY KEY (`id`)
@@ -46,8 +49,8 @@ DROP TABLE IF EXISTS `photos`;
 
 CREATE TABLE `photos` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `id_answer` INTEGER NOT NULL ,
-  `url` VARCHAR(255) NOT NULL ,
+  `id_answer` INTEGER NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -55,8 +58,8 @@ CREATE TABLE `photos` (
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `answer` ADD FOREIGN KEY (questions_id_question) REFERENCES `questions` (`questions_id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (id_answer) REFERENCES `answer` (`id`);
+ALTER TABLE `answer` ADD FOREIGN KEY (questions_id_question) REFERENCES `questions` (`questions_id`) ON DELETE CASCADE;
+ALTER TABLE `photos` ADD FOREIGN KEY (id_answer) REFERENCES `answer` (`id`) ON DELETE CASCADE;
 
 -- ---
 -- Table Properties
